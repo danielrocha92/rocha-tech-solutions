bb7e58d276632bc3e632258ce9a1ebae4c81641e
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2025-09-22 19:27:45 -0300

    Atualizar o HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import Hero from '../sections/Hero/Hero';
-import About from '../sections/About/About';
-import Services from '../sections/Services/Services';
-import Contact from '../sections/Contact/Contact';
-import useScrollToHash from '../hooks/useScrollToHash';
-import Portfolio from '../sections/Portfolio/Portfolio';
+import Hero from '../../sections/Hero/Hero';
+import About from '../../sections/About/About';
+import Services from '../../sections/Services/Services';
+import Portfolio from '../../sections/Portfolio/Portfolio';
+import HowItWorks from '../../sections/HowItWorks/HowItWorks';
+import Contact from '../../sections/Contact/Contact';
 
-function HomePage() {
-  useScrollToHash(80);
-
+const HomePage = () => {
   return (
     <>
       <Hero />
@@ -19,6 +17,7 @@ function HomePage() {
       <Contact />
     </>
   );
-}
+};
 
 export default HomePage;
+
