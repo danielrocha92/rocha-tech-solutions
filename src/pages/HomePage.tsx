59cc1f2edbc35ae3a8d3d1c2027ec82588b563de
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2025-09-22 19:34:38 -0300

    Atualizar o HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from '../sections/Hero/Hero';
 import About from '../sections/About/About';
 import Services from '../sections/Services/Services';
-import HowItWorks from '../sections/HowItWorks/HowItWorks';
+import HowItWorks from '../components/HowItWorks/HowItWorks';
 import Portfolio from '../sections/Portfolio/Portfolio';
 import Contact from '../sections/Contact/Contact';
 
