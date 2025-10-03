1abb95e967d5c42ae7cbe3586b918069f8117a09
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2025-10-03 19:12:48 -0300

    Atualizar o Hero.tsx

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -5,7 +5,7 @@ import './Hero.css';
 const Hero = () => {
   const [currentVideo, setCurrentVideo] = useState(0);
   const videoSources = [
-    "/assets/videos/https://g.co/gemini/share/ff678e685e22.mp4",
+    "/assets/videos/gemini_generated_video_.mp4",
     "/assets/videos/banner.mp4"
   ];
 
